4fc0fc61eabcd00498f6cdbb2b5126d73ee38930
Markus <niiranen.markus@gmail.com>
2021-02-11 21:58:56 +0200

    added functionality

---
+++ fill.js
@@ -0,0 +1,7 @@
+ 
+function fillpage() {
+  var wtext = document.querySelector("#text");
+  wtext.innerHTML = "hieno ompi sivu jos toimii";
+  var el = document.querySelector("#temp");
+  el.innerHTML = "ompi ompi on";
+}
