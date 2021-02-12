7adfab3e0f3f15602f69c2254d03cde7278d8196
Markus <niiranen.markus@gmail.com>
2021-02-12 13:07:50 +0200

    updated content

--- fill.js
+++ fill.js
@@ -1,7 +1,7 @@
  
 function fillpage() {
   var wtext = document.querySelector("#text");
-  wtext.innerHTML = "hieno ompi sivu jos toimii";
+  wtext.innerHTML = "Tähän tulee pottukellarin olosuhde tiedot";
   var el = document.querySelector("#temp");
-  el.innerHTML = "ompi ompi on";
+  el.innerHTML = "tähän lämpötila";
 }
