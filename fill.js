a7118850aa0869fef11b9b70a926e80407a7d102
Markus <niiranen.markus@gmail.com>
2021-02-16 22:32:08 +0200

    changed data source

--- fill.js
+++ fill.js
@@ -1,7 +1,10 @@
  
 function fillpage() {
-  var wtext = document.querySelector("#text");
-  wtext.innerHTML = "Tähän tulee pottukellarin olosuhde tiedot";
-  var el = document.querySelector("#temp");
-  el.innerHTML = "tähän lämpötila";
+  $.getJSON('https://thdbapi.herokuapp.com/api', function (data) {
+    // JSON result in `data` variable
+    var wtext = document.querySelector("#text");
+    wtext.innerHTML = "Tähän tulee pottukellarin olosuhde tiedot";
+    var el = document.querySelector("#temp");
+    el.innerHTML = "tähän lämpötila " + data;
+  });
 }
