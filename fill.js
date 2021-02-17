16843a71a6778110b439c3d7ff483c8a15479826
Markus <niiranen.markus@gmail.com>
2021-02-17 22:43:07 +0200

    added comments

--- fill.js
+++ fill.js
@@ -1,11 +1,12 @@
- 
+/*Create content to index.html with Javasript*/
 function fillpage() {
+  /*Get data from Heroku app api*/
   $.getJSON('https://thdbapi.herokuapp.com/api', function (data) {
     var wtext = document.querySelector("#text");
     wtext.innerHTML = "Tähän tulee pottukellarin olosuhde tiedot";
     var el = document.querySelector("#temp");
     el.innerHTML = "Uusin lämpötila: " + data[data.length-1][2] + String.fromCharCode(176) + "C ja suhteellinen kosteus: " + data[data.length-1][3] + "%";
-  
+    /*Create trace arrays from data */
     var trace1 = {
       x: [],
       y: [],
@@ -30,16 +31,6 @@ function fillpage() {
     for (var i = 0; i < data.length; i++){
      trace2.y.push(data[i][3]);
     }
-    
-      
-    /*
-    data.forEach(obj => {
-      Object.entries(obj).forEach(([key, value]) => {
-          el.innerHTML +=`${key};${value[2]} `;
-      });
-      console.log('-------------------');
-  });*/
-      
   var data = [trace1, trace2];
   
   Plotly.newPlot('plotData', data);
